ccf540e0cc94c4665c3a1357a4889599cc17f7fb
Watonnum <adamcorktail@gmail.com>
2024-04-29 20:40:00 +0700

    first release

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// write code here
+let pocket = 555;
+let mafamily = "suphap" + " and" + " kanokwan";
+let address = "290/130 ..........";
+let age_uni = "13.787±0.020 billion years";
+
+console.log(pocket);
+
+console.log(mafamily);
+
+console.log(address);
+
+console.log(age_uni);
